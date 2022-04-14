27c906b0dfe56ea72b6b0f51d029fe8e73d3ff62
moretza rezaeian <morteza78rezaeian@gmail.com>
2022-04-14 15:19:43 +0430

    init master

--- src/App.js
+++ src/App.js
@@ -3,31 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <div className='hero'>
-        <div className='circle'>
-          <span className='element ' style={{ '--i': 1 }}>
-            <b>one</b>
-          </span>
-          <span className='element ' style={{ '--i': 2 }}>
-            <b> two</b>
-          </span>
-          <span className='element ' style={{ '--i': 3 }}>
-            <b>three</b>
-          </span>
-          <span className='element ' style={{ '--i': 4 }}>
-            <b>four</b>
-          </span>
-          <span className='element ' style={{ '--i': 5 }}>
-            <b>five</b>
-          </span>
-          <span className='element ' style={{ '--i': 6 }}>
-            <b>six</b>
-          </span>
-          <span className='element ' style={{ '--i': 7 }}>
-            <b>seven</b>
-          </span>
-        </div>
-      </div>
+      
     </div>
   );
 }
