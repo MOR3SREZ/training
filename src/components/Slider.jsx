52e9048e0ac20b282d46e21d0f85b7bfc34e4163
moretza rezaeian <morteza78rezaeian@gmail.com>
2022-05-08 14:33:14 +0430

    done slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
-import {motion,useAnimation} from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion';
 
-
-import Card from './Card'
-import './Slider.css'
-
+import Card from './Card';
+import './Slider.css';
 
 const Slider = () => {
   const [width, setWidth] = useState(0);
 
   const carousel = useRef();
-  const slider = useRef()
+  const slider = useRef();
 
   const controls = useAnimation();
 
@@ -22,36 +20,37 @@ const Slider = () => {
   //   console.log('x',slider.current.scrollWidth)
   // })
 
+  const goStep = (dir) => {
+    trigger = trigger + dir * distance;
+    controls.start({ x: trigger });
+  };
 
-
-  const goStep = (dir)=>{
-    trigger = trigger+ (dir*distance);
-    controls.start({x:trigger});
-  }
-
-  const goLeft = ()=>{
-    if(Math.abs(trigger) >0){
-      goStep(+1)
+  const goLeft = () => {
+    if (Math.abs(trigger) > 0) {
+      goStep(+1);
     }
-  }
+  };
 
-  const goRight = ()=>{
-    if(Math.abs(trigger)+distance <=width){
-      goStep(-1)
+  const goRight = () => {
+    if (Math.abs(trigger) + distance <= width) {
+      goStep(-1);
     }
-  }
+  };
 
   useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
-  }, [])
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
 
   return (
     <motion.div className='carousel' ref={carousel}>
-        
-      <motion.div className="inner-carousel" ref={slider} drag='x' dragConstraints={{right:0, left:-width}} 
-      whileTap={{cursor:'grabbing'}}
-      animate={controls}
-      transition={{ duration:0.5 , ease:'easeInOut'}}
+      <motion.div
+        className='inner-carousel'
+        ref={slider}
+        drag='x'
+        dragConstraints={{ right: 0, left: -width }}
+        whileTap={{ cursor: 'grabbing' }}
+        animate={controls}
+        transition={{ duration: 0.5, ease: 'easeInOut' }}
       >
         <Card />
         <Card />
@@ -60,16 +59,37 @@ const Slider = () => {
         <Card />
         <Card />
       </motion.div>
-      
-        <motion.div className="left-arrow" onClick={goLeft} whileTap={{scale:0.9}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
-        </motion.div>
-        <motion.div className="right-arrow" onClick={goRight} whileTap={{scale:0.9}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
-        </motion.div>
-      
-    </motion.div>
-  )
-}
 
-export default Slider
+      <motion.div
+        className='left-arrow'
+        onClick={goLeft}
+        whileTap={{ scale: 0.9 }}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+        >
+          <path d='M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z' />
+        </svg>
+      </motion.div>
+      <motion.div
+        className='right-arrow'
+        onClick={goRight}
+        whileTap={{ scale: 0.9 }}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+        >
+          <path d='M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z' />
+        </svg>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Slider;
